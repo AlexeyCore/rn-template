506e453b0e5df1246ce1d2c44e3de9b46e4b7ee1
Bogaley Alexey <vuraddo@yahoo.com>
2017-06-19 19:32:19 +0700

    Add complete message

--- index.py
+++ index.py
@@ -2,10 +2,13 @@ from packages.modules import structure, helpers
 from packages.modules.state import g_state
 
 
-# Initialization react-native
+# Initialization react-native TODO: ask user about dependencies
 print('Initialization react-native...')
 init_complete = helpers.run_command(['react-native init %s' % g_state.get('name')])
 print(init_complete)
 
 # Restructuring react-native project
 structure.restructuring()
+
+# Complete
+print('\033[92mComplete!\033[0m')
