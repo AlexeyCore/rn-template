4da653f5b131d134afcc357c038a9d70f8911e95
Bogaley Alexey <vuraddo@yahoo.com>
2017-06-19 16:30:21 +0700

    Init react-native

--- index.py
+++ index.py
@@ -1,1 +1,16 @@
-print('HELLO')
+from packages.modules import structure, helpers
+from packages.modules.state import g_state
+
+
+def install_rn(project_name=''):
+    return helpers.run_command(['react-native init %s' % project_name])
+
+
+def init():
+    print('Initialization react-native...')
+    init_complete = install_rn(g_state.get('name'))
+    print(init_complete)
+
+
+# Run template scripts
+init()
